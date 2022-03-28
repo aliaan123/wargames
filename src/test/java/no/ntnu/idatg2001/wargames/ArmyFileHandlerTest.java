902da08b40e790edeb7632ced4758a1be70d6279
Aliaan <aliaan.azam02@gmail.com>
2022-03-28 14:09:49 +0200

    Implemented test class for file handling. Created tests and ran them to confirm file handling works.

---
+++ src/test/java/no/ntnu/idatg2001/wargames/ArmyFileHandlerTest.java
@@ -0,0 +1,56 @@
+package no.ntnu.idatg2001.wargames;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArmyFileHandlerTest {
+
+    private Army army;
+
+    @BeforeEach
+    void setUp() {
+
+        army = new Army("mandem");
+        army.add(new RangedUnit("ranged",100));
+        army.add(new InfantryUnit("inf",100));
+        army.add(new CavalryUnit("cav",100));
+        army.add(new CommanderUnit("com",100));
+
+    }
+
+    @Test
+    void testFailReadCSV()
+    {
+        army.getAllUnits().get(0).setHealth(50);
+        ArmyFileHandler.writeCSV(army, Path.of("units.csv"));
+        army.getAllUnits().get(0).setHealth(100);
+        Army readArmy = ArmyFileHandler.readCsv(Path.of("units.csv"));
+        assertEquals(army,readArmy);
+        assertEquals("mandem", readArmy.getName());
+        for(int i = 0; i<readArmy.getAllUnits().size(); i++)
+        {
+            Unit newUnit = readArmy.getAllUnits().get(i);
+            Unit ogUnit = army.getAllUnits().get(i);
+            if(newUnit.getHealth() == ogUnit.getHealth()) {
+                assertEquals(newUnit.getHealth(), ogUnit.getHealth());
+            }
+            else {
+                assertNotEquals(newUnit.getHealth(), ogUnit.getHealth());
+            }
+        }
+    }
+
+
+    @Test
+    void testWriteAndReadCSV()
+    {
+        ArmyFileHandler.writeCSV(army, Path.of("units.csv"));
+        Army readArmy = ArmyFileHandler.readCsv(Path.of("units.csv"));
+        assertEquals(army,readArmy);
+    }
+
+}
